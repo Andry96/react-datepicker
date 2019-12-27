1b502bb2b2b84f70849533e9cbf247bd80b504ff
Andrea <44727620+Andry96@users.noreply.github.com>
2019-12-27 14:19:23 +0100

    Full datepicker

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./components/Calendar/index";
+//import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./App";
 
-import "./styles.css";
-import Calendar from "./components/Calendar/index";
-
-function App() {
-  return (
-    <div className="App">
-      <Calendar />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
